b81ec0511f9f583726234333e634f76ea96c164d
Daniel Puzan <danpuz7@gmail.com>
2024-03-06 22:16:18 +0000

    main.tf fixed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "vpc_network" {
   project                 = "My First Project"
   name                    = "gcp_network"
-  routing_mode            = “GLOBAL”
+  routing_mode            = "GLOBAL"
   auto_create_subnetworks = true
 }
