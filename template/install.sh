6d0edb155a5a1167d7fa62c0c4daf0c1f03de3b5
Daniel Puzan <danpuz7@gmail.com>
2024-03-06 21:35:33 +0000

    update install file

--- template/install.sh
+++ template/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 # Download HashiCorp’s GPG key and add it to the keyring
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
 # Add the HashiCorp Linux repository
 echo “deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main” | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 # Update the apt package list and install Terraform
 sudo apt update && sudo apt install terraform
