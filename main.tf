37abe13b383516bb32791a26e6c9da04cb3a94c5
Daniel Puzan <danpuz7@gmail.com>
2024-03-06 22:20:00 +0000

    main fixed 4

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "vpc_network" {
   project                 = "just-zoo-416400"
-  name                    = "vpc_network"
+  name                    = "google_compute_network"
   routing_mode            = "GLOBAL"
   auto_create_subnetworks = true
 }
