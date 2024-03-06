21b764169abde0ee33021fc44b8be943036cc89b
Daniel Puzan <danpuz7@gmail.com>
2024-03-06 22:46:23 +0000

    main fix 9

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-module "vpc" {
+resource "vpc" {
   source  = "terraform-google-modules/network/google"
   version = "9.0.0"
-  
+
   project_id   = "just-zoo-416400"
   network_name = "vpc-network"
   routing_mode = "GLOBAL"
