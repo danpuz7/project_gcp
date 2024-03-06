7b45cbe62554e47437aac0d664b713d5fc40e78b
Daniel Puzan <danpuz7@gmail.com>
2024-03-06 22:54:39 +0000

    main fix 11

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 resource "google_compute_network" "vpc_network" {
-  source  = "terraform-google-modules/network/google"
-  version = "9.0.0"
-
-  project_id   = "just-zoo-416400"
-  network_name = "vpc-network"
-  routing_mode = "GLOBAL"
+  project                 = "just-zoo-416400"
+  name                    = "vpc-network"
   auto_create_subnetworks = true
+  mtu                     = 1460
+  routing_mode            = "GLOBAL"
 }
